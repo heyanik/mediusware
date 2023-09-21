eeebf1fbf84ddc08827a1b70427b74dd78073fa3
heyanik <anikdaspartha58@gmail.com>
2023-09-21 14:49:48 +0600

    finish ModalA & ModalB

--- src/components/ModalB.jsx
+++ src/components/ModalB.jsx
@@ -1,64 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-
-// const ModalB = ({ onClose, onlyEvenChecked }) => {
-//   const [usContacts, setUSContacts] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const allContactsResponse = await axios.get(
-//           "https://contact.mediusware.com/api/contacts/"
-//         );
-
-//         const usContactsData = allContactsResponse.data.results.filter(
-//           (contact) => contact.country.name === "United States"
-//         );
-
-//         setUSContacts(usContactsData);
-//         setLoading(false);
-//       } catch (error) {
-//         setError(error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   }
-
-//   if (error) {
-//     return <div>Error: {error.message}</div>;
-//   }
-
-//   return (
-//     <div>
-//       <h2>US Contacts</h2>
-//       <ul>
-//         {usContacts.map((contact) => (
-//           <li key={contact.id}>
-//             <strong>Contact ID:</strong> {contact.id}
-//             <br />
-//             <strong>Name:</strong> {contact.name}
-//             <br />
-//             <strong>Phone:</strong> {contact.phone}
-//             <br />
-//             <strong>Country:</strong> {contact.country.name}
-//           </li>
-//         ))}
-//       </ul>
-//       <button onClick={onClose}>Close</button>
-//     </div>
-//   );
-// };
-
-// export default ModalB;
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -70,12 +9,10 @@ const ModalB = ({ onClose, onAllContactsClick, onlyEvenChecked }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch all contacts from the "All Contacts" API
         const allContactsResponse = await axios.get(
           "https://contact.mediusware.com/api/contacts/"
         );
 
-        // Filter contacts to include only those with the country name 'US'
         const usContactsData = allContactsResponse.data.results.filter(
           (contact) => contact.country.name === "United States"
         );
